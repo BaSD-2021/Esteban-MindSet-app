c9c2b66a5ccecd04fefc9a3326fab95e75c03194
Naila Ghigo <nailaghigo20@gmail.com>
2021-11-26 13:47:02 -0300

    MS-58: Create admins table, give some CSS and add GET and DELETE methods

--- src/Components/Admins/index.js
+++ src/Components/Admins/index.js
@@ -1,11 +1,69 @@
+import { useEffect, useState } from 'react';
 import styles from './admins.module.css';
 
-function Admins() {
+const Admins = () => {
+  const [admins, setAdmins] = useState([]);
+
+  console.log('admins', admins);
+
+  const getAdmins = () => {
+    fetch(`${process.env.REACT_APP_API}/admins`)
+      .then((response) => response.json())
+      .then((response) => {
+        setAdmins(response.data);
+      });
+  };
+
+  useEffect(() => {
+    getAdmins();
+  }, []);
+
+  const updateAdmin = () => {
+    window.location.pathname = `/admins/Form`;
+  };
+
+  const deleteAdmin = (id) => {
+    fetch(`${process.env.REACT_APP_API}/admins/${id}`, { method: 'DELETE' }).then(() => {
+      getAdmins();
+    });
+  };
+
   return (
     <section className={styles.container}>
       <h2>Admins</h2>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>User Name</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {admins.map((admin, index) => {
+              return (
+                <tr onClick={updateAdmin} key={index}>
+                  <td>{admin.name}</td>
+                  <td>{admin.username}</td>
+                  <td>
+                    <button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        deleteAdmin(admin._id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
-}
+};
 
 export default Admins;
