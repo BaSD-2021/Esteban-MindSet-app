465a34fc311d421950b175189a23020c71f5cfb9
ortizjoaquin <joaquin.ortizdlc@gmail.com>
2021-11-26 22:21:06 -0300

    MS-60: postulants list displaying

--- src/Components/Postulants/index.js
+++ src/Components/Postulants/index.js
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import styles from './postulants.module.css';
 
 function Postulants() {
+  const [postulants, setPostulants] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/postulants`)
+      .then((response) => response.json())
+      .then((response) => {
+        setPostulants(response.data);
+      });
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>Postulants</h2>
+      <ul>
+        {postulants.map((postulant) => {
+          return (
+            <li key={postulant._id}>
+              {postulant.firstName}
+              {postulant.lastName}
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 }
