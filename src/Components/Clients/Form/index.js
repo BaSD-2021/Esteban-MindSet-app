67189f04ec79ab7d24210341b98118965c553636
Morabito Federico <morabitofj@gmail.com>
2021-11-29 01:13:39 -0300

    MS-72: add client resource form and modal

---
+++ src/Components/Clients/Form/index.js
@@ -0,0 +1,186 @@
+import { useEffect, useState } from 'react';
+import styles from './form.module.css';
+function Form() {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [country, setCountry] = useState('');
+  const [state, setState] = useState('');
+  const [city, setCity] = useState('');
+  const [address, setAddress] = useState('');
+  const [logo, setLogo] = useState('');
+  const [description, setDescription] = useState('');
+
+  const params = new URLSearchParams(window.location.search);
+  const clientId = params.get('_id');
+  let fetchMethod = 'POST';
+
+  const onLoading = (data) => {
+    setName(data.data[0].name ? data.data[0].name : '');
+    setPhone(data.data[0].phone ? data.data[0].phone : '');
+    setCountry(data.data[0].location ? data.data[0].location.country : '');
+    setState(data.data[0].location ? data.data[0].location.state : '');
+    setCity(data.data[0].location ? data.data[0].location.city : '');
+    setAddress(data.data[0].location ? data.data[0].location.address : '');
+    setLogo(data.data[0].logo ? data.data[0].logo : '');
+    setDescription(data.data[0].description ? data.data[0].description : '');
+  };
+  const onChangeName = (event) => {
+    setName(event.target.value);
+  };
+  const onChangePhone = (event) => {
+    setPhone(event.target.value);
+  };
+  const onChangeCountry = (event) => {
+    setCountry(event.target.value);
+  };
+  const onChangeState = (event) => {
+    setState(event.target.value);
+  };
+  const onChangeCity = (event) => {
+    setCity(event.target.value);
+  };
+  const onChangeAddress = (event) => {
+    setAddress(event.target.value);
+  };
+  const onChangeLogo = (event) => {
+    setLogo(event.target.value);
+  };
+  const onChangeDescription = (event) => {
+    setDescription(event.target.value);
+  };
+
+  if (clientId) {
+    fetchMethod = 'PUT';
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const params = new URLSearchParams(window.location.search);
+    const clientId = params.get('_id');
+    const options = {
+      method: fetchMethod,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        phone: parseInt(phone),
+        location: {
+          country: country,
+          state: state,
+          city: city,
+          address: address
+        },
+        logo: logo,
+        description: description
+      })
+    };
+    const url = clientId
+      ? `${process.env.REACT_APP_API}/clients/${clientId}`
+      : `${process.env.REACT_APP_API}/clients/`;
+    fetch(url, options)
+      .then((response) => {
+        if (response.status !== 200 && response.status !== 201) {
+          return response.json().then(({ message }) => {
+            throw new Error(message);
+          });
+        }
+        return response.json();
+      })
+      .then(() => {
+        window.location.href = '/clients';
+      })
+      .catch((error) => {
+        console.log('err', error);
+      });
+  };
+
+  useEffect(() => {
+    if (clientId) {
+      fetch(`${process.env.REACT_APP_API}/clients?_id=${clientId}`)
+        .then((response) => response.json())
+        .then((res) => {
+          onLoading(res);
+        });
+    }
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className={styles.container}>
+        <h2>Form</h2>
+        <label>
+          <span>Name</span>
+        </label>
+        <input id="name" name="name" type="text" required value={name} onChange={onChangeName} />
+        <label>
+          <span>Phone</span>
+        </label>
+        <input
+          id="phone"
+          name="phone"
+          type="number"
+          required
+          value={phone}
+          onChange={onChangePhone}
+        />
+        <label>
+          <span>Country</span>
+        </label>
+        <input
+          id="country"
+          name="country"
+          type="text"
+          required
+          value={country}
+          onChange={onChangeCountry}
+        />
+        <label>
+          <span>State</span>
+        </label>
+        <input
+          id="state"
+          name="state"
+          type="text"
+          required
+          value={state}
+          onChange={onChangeState}
+        />
+        <label>
+          <span>City</span>
+        </label>
+        <input id="city" name="city" required type="text" value={city} onChange={onChangeCity} />
+        <label>
+          <span>Address</span>
+        </label>
+        <input
+          id="address"
+          name="address"
+          required
+          type="text"
+          value={address}
+          onChange={onChangeAddress}
+        />
+        <label>
+          <span>Logo</span>
+        </label>
+        <input id="logo" name="logo" type="text" value={logo} onChange={onChangeLogo} />
+        <label>
+          <span>Description</span>
+        </label>
+        <input
+          id="description"
+          name="description"
+          type="text"
+          value={description}
+          onChange={onChangeDescription}
+        />
+        <button id="saveButton" type="submit">
+          Save
+        </button>
+      </form>
+    </div>
+  );
+}
+export default Form;
---
+++ src/Components/Clients/Modal/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './modal.module.css';
+
+function Modal(props) {
+  if (!props.show) {
+    return null;
+  }
+  return (
+    <section id="modal-section" className={styles.container}>
+      <div id="modal-content" className={styles.window}>
+        <h2 id="modal-title">Delete item</h2>
+        <p>YOU ARE ABOUT TO DELETE A CLIENT</p>
+        <div className={styles.buttonContainer}>
+          <button id="cancel-button" onClick={props.closeModal}>
+            Cancel
+          </button>
+          <button
+            id="proceed-button"
+            onClick={(event) => {
+              props.function(event, props.id);
+            }}
+          >
+            Confirm
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Modal;
