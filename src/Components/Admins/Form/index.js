9bb1726f7e0ac1f6a997d7ad9ff084520fcb8c16
Naila Ghigo <nailaghigo20@gmail.com>
2021-12-03 01:01:18 -0300

    MS-58: Change some PR corrections

--- src/Components/Admins/Form/index.js
+++ src/Components/Admins/Form/index.js
@@ -5,13 +5,13 @@ import Modal from '../modal/modal';
 const params = new URLSearchParams(window.location.search);
 const adminId = params.get('_id');
 let fetchMethod = 'POST';
-let url = `${process.env.REACT_APP_API}/admins`;
 
 function Form() {
   const [nameValue, setNameValue] = useState('');
   const [usernameValue, setUsernameValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
   const [error, setError] = useState(false);
+  // const [isLoading, setLoading] = useState(false);
 
   const getAdminById = () => {
     fetch(`${process.env.REACT_APP_API}/admins?_id=${adminId}`)
@@ -29,14 +29,15 @@ function Form() {
         setUsernameValue(username);
         setPasswordValue(password);
       })
-      .catch((error) => {
-        console.log('err', error);
+      .catch((err) => {
+        setError(err.toString());
       });
   };
 
+  let url;
   useEffect(() => {
     if (adminId) {
-      url += `/${adminId}`;
+      url = `${process.env.REACT_APP_API}/admins?_id=${adminId}`;
       fetchMethod = 'PUT';
       getAdminById();
     }
@@ -44,7 +45,8 @@ function Form() {
 
   const save = (e) => {
     e.preventDefault();
-    fetch(url, {
+
+    const options = {
       method: fetchMethod,
       headers: {
         'Content-Type': 'application/json'
@@ -54,14 +56,28 @@ function Form() {
         username: usernameValue,
         password: passwordValue
       })
-    })
+    };
+
+    if (adminId) {
+      fetchMethod = 'PUT';
+      url = `${process.env.REACT_APP_API}/admins/${adminId}`;
+    } else {
+      fetchMethod = 'POST';
+      url = `${process.env.REACT_APP_API}/admins`;
+      getAdminById();
+    }
+    fetch(url, options)
       .then((response) => {
         if (response.status !== 200 && response.status !== 201) {
           return response.json().then(({ message }) => {
             throw new Error(message);
           });
         }
+        return response.json();
+      })
+      .then(() => {
         window.location.href = '/admins';
+        getAdminById();
       })
       .catch((err) => {
         setError(err);
@@ -111,7 +127,7 @@ function Form() {
       </button>
       {error && (
         <Modal>
-          {error}
+          {`${error}`}
           <button className={styles.button} onClick={() => setError(false)}>
             Close
           </button>
