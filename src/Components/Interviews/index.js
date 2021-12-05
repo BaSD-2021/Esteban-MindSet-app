1cd0f141f393e5e9b5e4efd3067f09e850954d4f
Morabito Federico <morabitofj@gmail.com>
2021-12-05 13:45:28 -0300

    MS-85: update interview loading

--- src/Components/Interviews/index.js
+++ src/Components/Interviews/index.js
@@ -7,9 +7,11 @@ function Interviews() {
   const [showModal, setShowModal] = useState(false);
   const [interviews, saveInterviews] = useState([]);
   const [idToDelete, setIdToDelete] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
   const history = useHistory();
 
   const deleteInterview = (id) => {
+    setIsLoading(true);
     const url = `${process.env.REACT_APP_API}/interviews/${id}`;
     fetch(url, {
       method: 'DELETE'
@@ -21,11 +23,15 @@ function Interviews() {
         })
         .catch((error) => {
           console.log('err', error);
+        })
+        .finally(() => {
+          setIsLoading(false);
         });
     });
   };
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(`${process.env.REACT_APP_API}/interviews`)
       .then((response) => response.json())
       .then((response) => {
@@ -33,6 +39,9 @@ function Interviews() {
       })
       .catch((error) => {
         console.log('err', error);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, []);
 
@@ -50,43 +59,47 @@ function Interviews() {
   return (
     <section className={styles.container}>
       <h2>Interviews</h2>
-      <table className={styles.table}>
-        <thead className={styles.thead}>
-          <tr>
-            <td className={styles.td}>Postulant</td>
-            <td className={styles.td}>Client</td>
-            <td className={styles.td}>Status</td>
-            <td className={styles.td}>Date</td>
-            <td className={styles.td}>Delete</td>
-          </tr>
-        </thead>
-        <tbody className={styles.tbody}>
-          {interviews.map((interview) => {
-            return (
-              <tr
-                onClick={() => history.push(`/interviews/form?_id=${interview._id}`)}
-                key={interview._id}
-              >
-                <td className={styles.td}>{interview.postulant?.firstName || '-'}</td>
-                <td className={styles.td}>{interview.client?.name || '-'}</td>
-                <td className={styles.td}>{interview.status}</td>
-                <td className={styles.td}>{interview.date}</td>
-                <td className={styles.td}>
-                  <button
-                    type="button"
-                    onClick={(e) => {
-                      preventAndShow(e, interview._id);
-                    }}
-                    className={styles.deleteButton}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {isLoading ? (
+        <p className={styles.loading}>On Loading ...</p>
+      ) : (
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr>
+              <td className={styles.td}>Postulant</td>
+              <td className={styles.td}>Client</td>
+              <td className={styles.td}>Status</td>
+              <td className={styles.td}>Date</td>
+              <td className={styles.td}>Delete</td>
+            </tr>
+          </thead>
+          <tbody className={styles.tbody}>
+            {interviews.map((interview) => {
+              return (
+                <tr
+                  onClick={() => history.push(`/interviews/form?_id=${interview._id}`)}
+                  key={interview._id}
+                >
+                  <td className={styles.td}>{interview.postulant?.firstName || '-'}</td>
+                  <td className={styles.td}>{interview.client?.name || '-'}</td>
+                  <td className={styles.td}>{interview.status}</td>
+                  <td className={styles.td}>{interview.date}</td>
+                  <td className={styles.td}>
+                    <button
+                      type="button"
+                      onClick={(e) => {
+                        preventAndShow(e, interview._id);
+                      }}
+                      className={styles.deleteButton}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
       <Modal id={idToDelete} function={deleteInterview} show={showModal} closeModal={closeModal} />
       <Link to="/Interviews/Form" className={styles.button}>
         ADD INTERVIEW
