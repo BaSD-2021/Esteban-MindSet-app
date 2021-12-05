7f38aca46a30e644b41cb92e2889b75679795879
Morabito Federico <morabitofj@gmail.com>
2021-12-05 13:40:30 -0300

    MS-85: update clients loading

--- src/Components/Clients/index.js
+++ src/Components/Clients/index.js
@@ -8,9 +8,11 @@ function Clients() {
   const [idToDelete, setIdToDelete] = useState('');
   const [clients, setClients] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
   const history = useHistory();
 
   const deleteClient = (event, id) => {
+    setIsLoading(true);
     const url = `${process.env.REACT_APP_API}/clients/${id}`;
     fetch(url, {
       method: 'DELETE'
@@ -34,6 +36,7 @@ function Clients() {
       })
       .finally(() => {
         closeModal();
+        setIsLoading(false);
       });
   };
 
@@ -49,6 +52,7 @@ function Clients() {
   };
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(`${process.env.REACT_APP_API}/clients`)
       .then((response) => {
         if (response.status !== 200) {
@@ -63,46 +67,53 @@ function Clients() {
       })
       .catch((err) => {
         setErrorMessage(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <section className={styles.container}>
       <h2 className={styles.subtitle}>Clients</h2>
-      <table className={styles.tableData}>
-        <thead className={styles.tableHeader}>
-          <tr className={styles.tdStyles}>
-            <th>Name</th>
-            <th>Phone</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {clients.map((client) => {
-            return (
-              <tr
-                onClick={() => history.push(`/clients/form?_id=${client._id}`)}
-                key={client._id}
-                className={styles.trStyles}
-              >
-                <td className={styles.tdStyles}>{client.name ? client.name : '-'}</td>
-                <td className={styles.tdStyles}>{client.phone ? client.phone : '-'}</td>
-                <td className={styles.tdStyles}>
-                  <button
-                    type="button"
-                    onClick={(e) => {
-                      preventAndShow(e, client._id);
-                    }}
-                    className={styles.buttonDelete}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {isLoading ? (
+        <p className={styles.loading}>On Loading ...</p>
+      ) : (
+        <table className={styles.tableData}>
+          <thead className={styles.tableHeader}>
+            <tr className={styles.tdStyles}>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clients.map((client) => {
+              return (
+                <tr
+                  onClick={() => history.push(`/clients/form?_id=${client._id}`)}
+                  key={client._id}
+                  className={styles.trStyles}
+                >
+                  <td className={styles.tdStyles}>{client.name ? client.name : '-'}</td>
+                  <td className={styles.tdStyles}>{client.phone ? client.phone : '-'}</td>
+                  <td className={styles.tdStyles}>
+                    <button
+                      type="button"
+                      onClick={(e) => {
+                        preventAndShow(e, client._id);
+                      }}
+                      className={styles.buttonDelete}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
       <div className={styles.errorMessage}>{errorMessage.message}</div>
       <Modal id={idToDelete} function={deleteClient} show={showModal} closeModal={closeModal} />
       <Link to="/Clients/Form" className={styles.buttonCreate}>
