4e1526427a0b4c5933c63083ad5f933bb7eb293a
Morabito Federico <morabitofj@gmail.com>
2021-12-06 08:20:34 -0300

    MS-85: add table component on application

--- src/Components/Applications/index.js
+++ src/Components/Applications/index.js
@@ -3,6 +3,7 @@ import styles from './applications.module.css';
 import Modal from '../Shared/Modal';
 import Button from '../Shared/Button';
 import { Link, useHistory } from 'react-router-dom';
+import Table from '../Shared/Table/index';
 
 function Applications() {
   const [showModal, setShowModal] = useState(false);
@@ -11,6 +12,10 @@ function Applications() {
   const [errorMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const history = useHistory();
+  const [infoToShow, setInfoToShow] = useState([]);
+  const [idToPass, setIdToPass] = useState([]);
+
+  const columnName = ['Job Description', 'Postulant', 'Interview', 'Result', 'Actions'];
 
   const deleteApplication = () => {
     setIsLoading(true);
@@ -64,6 +69,7 @@ function Applications() {
       })
       .then((response) => {
         setApplications(response.data);
+        setInformationToShow(response);
       })
       .catch((error) => {
         setErrorMessage(error);
@@ -73,57 +79,38 @@ function Applications() {
       });
   }, []);
 
+  const setInformationToShow = (data) => {
+    const dataToPass = [];
+    data.data.map((row) => {
+      idToPass.push(row._id);
+      dataToPass.push([
+        row.positions ? row.positions.jobDescription : '-',
+        row.postulants ? row.postulants.firstName + ' ' + row.postulants?.lastName : '-',
+        row.interview ? row.interview.date.slice(0, 10) : '-',
+        row.result ? row.result : '-'
+      ]);
+    });
+    setInfoToShow(dataToPass);
+    setIdToPass(idToPass);
+  };
+
+  const redirect = (id) => {
+    history.push(`/applications/form?_id=${id}`);
+  };
+
   return (
     <section className={styles.container}>
       <h2 className={styles.subtitle}>Applications</h2>
       {isLoading ? (
         <p className={styles.loading}>On Loading ...</p>
       ) : (
-        <table className={styles.tableData}>
-          <thead className={styles.tableHeader}>
-            <tr className={styles.trStyles}>
-              <th>Job Description</th>
-              <th>Postulant</th>
-              <th>Interview</th>
-              <th>Result</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {applications.map((application) => {
-              return (
-                <tr
-                  onClick={() => history.push(`/applications/form?_id=${application._id}`)}
-                  key={application._id}
-                  className={styles.trStyles}
-                >
-                  <td className={styles.tdStyles}>
-                    {application.positions ? application.positions.jobDescription : '-'}
-                  </td>
-                  <td className={styles.tdStyles}>
-                    {application.postulants
-                      ? application.postulants.firstName + ' ' + application.postulants.lastName
-                      : '-'}
-                  </td>
-                  <td className={styles.tdStyles}>
-                    {application.interview ? application.interview.date.substr(0, 10) : '-'}
-                  </td>
-                  <td className={styles.tdStyles}>
-                    {application.result ? application.result : '-'}
-                  </td>
-                  <td className={styles.tdStyles}>
-                    <Button
-                      name="deleteButton"
-                      onClick={(e) => {
-                        preventAndShow(e, application._id);
-                      }}
-                    />
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <Table
+          columnsName={columnName}
+          id={idToPass}
+          tableInfo={infoToShow}
+          deleteFunction={preventAndShow}
+          redirectFunction={redirect}
+        />
       )}
       <div id="error_message" className={styles.errorMessage}>
         {errorMessage.message}
