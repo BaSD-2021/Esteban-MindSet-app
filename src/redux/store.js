d15c8ae5af257a896474e5ef20e4d1e208ed306f
Verona Baudracco <veronabaudracco@gmail.com>
2021-12-09 01:26:47 -0300

    MS-95: add to admins reducer an initial state and reducer definition to avoid console error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-// import adminsReducer from './admins/reducer';
-// import applicationsReducer from './applications/reducer';
-// import clientsReducer from './clients/reducer';
-// import interviewsReducer from './interviews/reducer';
-// import positionsReducer from './positions/reducer';
-// import postulantsReducer from './postulants/reducer';
-// import profilesReducer from './profiles/reducer';
-// import psychologistsReducers from './psychologists/reducer';
-// import sessionsReducer from './sessions/reducer';
+import adminsReducer from './admins/reducer';
 
 const reducers = combineReducers({
-  // admins: adminsReducer,
-  // applications: applicationsReducer,
-  // clients: clientsReducer,
-  // interviews: interviewsReducer,
-  // positions: positionsReducer,
-  // postulants: postulantsReducer,
-  // profiles: profilesReducer,
-  // psychologists: psychologistsReducers,
-  // sessions: sessionsReducer
+  admins: adminsReducer
 });
 
 const enhancer = composeWithDevTools(applyMiddleware(thunk));
