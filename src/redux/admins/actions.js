6b81c81ba15a21c2761d2c9971c1c5846006ee18
Verona Baudracco <veronabaudracco@gmail.com>
2021-12-13 19:29:25 -0300

    MS-96: implement redux in delete and get methods

---
+++ src/redux/admins/actions.js
@@ -0,0 +1,137 @@
+// Action type constants definition
+export const GET_ADMINS_PENDING = 'GET_ADMINS_PENDING';
+export const GET_ADMINS_SUCCESS = 'GET_ADMINS_SUCCESS';
+export const GET_ADMINS_ERROR = 'GET_ADMINS_ERROR';
+
+export const GET_ADMIN_BY_ID_PENDING = 'GET_ADMIN_BY_ID_PENDING';
+export const GET_ADMIN_BY_ID_SUCCESS = 'GET_ADMIN_BY_ID_SUCCESS';
+export const GET_ADMIN_BY_ID_ERROR = 'GET_ADMIN_BY_ID_ERROR';
+
+export const CREATE_ADMIN_PENDING = 'CREATE_ADMIN_PENDING';
+export const CREATE_ADMIN_SUCCESS = 'CREATE_ADMIN_SUCCESS';
+export const CREATE_ADMIN_ERROR = 'CREATE_ADMIN_ERROR';
+
+export const UPDATE_ADMIN_PENDING = 'UPDATE_ADMIN_PENDING';
+export const UPDATE_ADMIN_SUCCESS = 'UPDATE_ADMIN_SUCCESS';
+export const UPDATE_ADMIN_ERROR = 'UPDATE_ADMIN_ERROR';
+
+export const DELETE_ADMIN_PENDING = 'DELETE_ADMIN_PENDING';
+export const DELETE_ADMIN_SUCCESS = 'DELETE_ADMIN_SUCCESS';
+export const DELETE_ADMIN_ERROR = 'DELETE_ADMIN_ERROR';
+
+export const CLEAN_ERROR = 'CLEAN_ERROR';
+
+export const CLEAN_SELECTED_ITEM = 'CLEAN_SELECTED_ITEM';
+
+// Actions definition
+export const getAdminsPending = () => {
+  return {
+    type: GET_ADMINS_PENDING
+  };
+};
+
+export const getAdminsSuccess = (data) => {
+  return {
+    type: GET_ADMINS_SUCCESS,
+    payload: data
+  };
+};
+
+export const getAdminsError = (error) => {
+  return {
+    type: GET_ADMINS_ERROR,
+    payload: error
+  };
+};
+
+export const getAdminByIdPending = () => {
+  return {
+    type: GET_ADMIN_BY_ID_PENDING
+  };
+};
+
+export const getAdminByIdSuccess = (data) => {
+  return {
+    type: GET_ADMIN_BY_ID_SUCCESS,
+    payload: data
+  };
+};
+
+export const getAdminByIdError = (error) => {
+  return {
+    type: GET_ADMIN_BY_ID_ERROR,
+    payload: error
+  };
+};
+
+export const createAdminPending = () => {
+  return {
+    type: CREATE_ADMIN_PENDING
+  };
+};
+
+export const createAdminSuccess = (data) => {
+  return {
+    type: CREATE_ADMIN_SUCCESS,
+    payload: data
+  };
+};
+
+export const createAdminError = (error) => {
+  return {
+    type: CREATE_ADMIN_ERROR,
+    payload: error
+  };
+};
+
+export const updateAdminPending = () => {
+  return {
+    type: UPDATE_ADMIN_PENDING
+  };
+};
+
+export const updateAdminSuccess = (data) => {
+  return {
+    type: UPDATE_ADMIN_SUCCESS,
+    payload: data
+  };
+};
+
+export const updateAdminError = (error) => {
+  return {
+    type: UPDATE_ADMIN_ERROR,
+    payload: error
+  };
+};
+
+export const deleteAdminPending = () => {
+  return {
+    type: DELETE_ADMIN_PENDING
+  };
+};
+
+export const deleteAdminSuccess = (id) => {
+  return {
+    type: DELETE_ADMIN_SUCCESS,
+    payload: id
+  };
+};
+
+export const deleteAdminError = (error) => {
+  return {
+    type: DELETE_ADMIN_ERROR,
+    payload: error
+  };
+};
+
+export const cleanError = () => {
+  return {
+    type: CLEAN_ERROR
+  };
+};
+
+export const cleanSelectedItem = () => {
+  return {
+    type: CLEAN_SELECTED_ITEM
+  };
+};
--- src/redux/admins/reducer.js
+++ src/redux/admins/reducer.js
@@ -1,11 +1,148 @@
+import {
+  GET_ADMINS_PENDING,
+  GET_ADMINS_SUCCESS,
+  GET_ADMINS_ERROR,
+  GET_ADMIN_BY_ID_PENDING,
+  GET_ADMIN_BY_ID_SUCCESS,
+  GET_ADMIN_BY_ID_ERROR,
+  CREATE_ADMIN_PENDING,
+  CREATE_ADMIN_SUCCESS,
+  CREATE_ADMIN_ERROR,
+  UPDATE_ADMIN_PENDING,
+  UPDATE_ADMIN_SUCCESS,
+  UPDATE_ADMIN_ERROR,
+  DELETE_ADMIN_PENDING,
+  DELETE_ADMIN_SUCCESS,
+  DELETE_ADMIN_ERROR,
+  CLEAN_ERROR
+} from './actions';
+
 const initialState = {
   isFetching: false,
   list: [],
+  selectedItem: {},
   error: ''
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_ADMINS_PENDING: {
+      return {
+        ...state,
+        isFetching: true,
+        error: initialState.error
+      };
+    }
+    case GET_ADMINS_SUCCESS: {
+      return {
+        ...state,
+        isFetching: false,
+        list: action.payload
+      };
+    }
+    case GET_ADMINS_ERROR: {
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
+    }
+    case GET_ADMIN_BY_ID_PENDING: {
+      return {
+        ...state,
+        isFetching: true,
+        error: initialState.error,
+        selectedItem: initialState.selectedItem
+      };
+    }
+    case GET_ADMIN_BY_ID_SUCCESS: {
+      return {
+        ...state,
+        isFetching: false,
+        selectedItem: action.payload
+      };
+    }
+    case GET_ADMIN_BY_ID_ERROR: {
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
+    }
+    case CREATE_ADMIN_PENDING: {
+      return {
+        ...state,
+        isFetching: true,
+        error: initialState.error
+      };
+    }
+    case CREATE_ADMIN_SUCCESS: {
+      return {
+        ...state,
+        isFetching: false,
+        list: [...state.list, action.payload]
+      };
+    }
+    case CREATE_ADMIN_ERROR: {
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
+    }
+    case UPDATE_ADMIN_PENDING: {
+      return {
+        ...state,
+        isFetching: true,
+        error: initialState.error
+      };
+    }
+    case UPDATE_ADMIN_SUCCESS: {
+      return {
+        ...state,
+        isFetching: false,
+        list: state.list.map((item) => {
+          if (item._id === action.payload._id) {
+            return action.payload;
+          }
+          return item;
+        })
+      };
+    }
+    case UPDATE_ADMIN_ERROR: {
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
+    }
+    case DELETE_ADMIN_PENDING: {
+      return {
+        ...state,
+        isFetching: true,
+        error: initialState.error
+      };
+    }
+    case DELETE_ADMIN_SUCCESS: {
+      return {
+        ...state,
+        isFetching: false,
+        list: state.list.filter((item) => item._id !== action.payload)
+      };
+    }
+    case DELETE_ADMIN_ERROR: {
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
+    }
+    case CLEAN_ERROR: {
+      return {
+        ...state,
+        error: initialState.error
+      };
+    }
     default: {
       return state;
     }
