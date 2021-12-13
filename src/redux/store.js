cf7ab8eb45d78f09fa47917dba57763656cd8440
Morabito Federico <morabitofj@gmail.com>
2021-12-12 23:07:48 -0300

    MS-107: add session reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import adminsReducer from './admins/reducer';
+import sessionsReducer from './sessions/reducer';
 
 const reducers = combineReducers({
-  admins: adminsReducer
+  admins: adminsReducer,
+  sessions: sessionsReducer
 });
 
 const enhancer = composeWithDevTools(applyMiddleware(thunk));
