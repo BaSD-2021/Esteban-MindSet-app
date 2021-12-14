8e0422c396e21d3432c00e17a994a972ec9e7c8f
Agustin Carthery <acarthery@Agustins-MacBook-Pro.local>
2021-12-13 23:36:47 -0300

    MS-104:getPsychologist and deletePsychologists thunks

--- src/Components/Psychologists/index.js
+++ src/Components/Psychologists/index.js
@@ -1,43 +1,58 @@
 import styles from './psychologists.module.css';
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import List from './List';
 import Modal from '../Shared/Modal';
 import ModalAvailability from './ModalAvailability';
 import AvailabilityTable from './AvailabilityTable';
+import { getPsychologists, deletePsychologist } from '../../redux/psychologists/thunks';
+//import { cleanError } from '../../redux/psychologists/actions';
 
 function Psychologists() {
-  const [psychologists, setPsychologist] = useState([]);
+  //const [psychologists, setPsychologist] = useState([]);
   const [idToDelete, setIdToDelete] = useState('');
   const [showAvailabilityModal, setShowAvailabilityModal] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
   const [psychologistAvailability, setPsychologistAvailability] = useState({});
   const [showError, setShowError] = useState(false);
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
+  //const [error, setError] = useState('');
+  //const [isLoading, setIsLoading] = useState(true);
   const history = useHistory();
 
+  const dispatch = useDispatch();
+
+  const psychologists = useSelector((store) => store.psychologists.list);
+  const error = useSelector((store) => store.psychologists.error);
+  const isLoading = useSelector((store) => store.psychologists.isFetching);
+
+  // useEffect(() => {
+  //   getPsychologists();
+  // }, []);
+
   useEffect(() => {
-    getPsychologists();
-  }, []);
+    if (!psychologists.length) {
+      dispatch(getPsychologists());
+    }
+  }, [psychologists]);
 
-  const getPsychologists = () => {
-    setIsLoading(true);
-    fetch(`${process.env.REACT_APP_API}/psychologists`)
-      .then((response) => response.json())
-      .then((response) => {
-        setPsychologist(response.data);
-      })
-      .catch(() => getError('There has been an error while retrieving psychologists'))
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
+  // const getPsychologists = () => {
+  //   setIsLoading(true);
+  //   fetch(`${process.env.REACT_APP_API}/psychologists`)
+  //     .then((response) => response.json())
+  //     .then((response) => {
+  //       setPsychologist(response.data);
+  //     })
+  //     .catch(() => getError('There has been an error while retrieving psychologists'))
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // };
 
-  const getError = (error) => {
-    setError(error);
-    setShowError(true);
-  };
+  // const getError = (error) => {
+  //   setError(error);
+  //   setShowError(true);
+  // };
 
   const toggleAvailabilityModal = (e, psychologist) => {
     if (!showAvailabilityModal) {
@@ -63,29 +78,29 @@ function Psychologists() {
     history.push(`/psychologists/form?_id=${id}`);
   };
 
-  const handleDelete = () => {
-    setIsLoading(true);
-    const url = `${process.env.REACT_APP_API}/psychologists/${idToDelete}`;
-    fetch(url, {
-      method: 'DELETE',
-      headers: {
-        'Content-type': 'application/json'
-      }
-    })
-      .then((res) => {
-        if (res.status !== 204) {
-          return res.json().then((message) => {
-            throw new Error(message);
-          });
-        }
-        toggleConfirmModal();
-        getPsychologists();
-      })
-      .catch((error) => error)
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
+  // const handleDelete = () => {
+  //   setIsLoading(true);
+  //   const url = `${process.env.REACT_APP_API}/psychologists/${idToDelete}`;
+  //   fetch(url, {
+  //     method: 'DELETE',
+  //     headers: {
+  //       'Content-type': 'application/json'
+  //     }
+  //   })
+  //     .then((res) => {
+  //       if (res.status !== 204) {
+  //         return res.json().then((message) => {
+  //           throw new Error(message);
+  //         });
+  //       }
+  //       toggleConfirmModal();
+  //       getPsychologists();
+  //     })
+  //     .catch((error) => error)
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // };
 
   return (
     <section className={styles.container}>
@@ -97,7 +112,6 @@ function Psychologists() {
           <List
             psychologists={psychologists}
             handleEdit={redirect}
-            handleDelete={handleDelete}
             toggleAvailabilityModal={toggleAvailabilityModal}
             toggleConfirmModal={toggleConfirmModal}
           />
@@ -113,7 +127,12 @@ function Psychologists() {
             title="Do you want to proceed and delete this psychologist?"
             onClose={toggleConfirmModal}
             isLoading={isLoading}
-            onConfirm={handleDelete}
+            onConfirm={() => {
+              dispatch(deletePsychologist(idToDelete)).then(() => {
+                setIdToDelete(undefined);
+                setShowConfirmModal(false);
+              });
+            }}
           />
         )}
         {showError && (
