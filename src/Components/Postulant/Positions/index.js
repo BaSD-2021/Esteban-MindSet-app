00977a035c9c3b981596db36383dc38fb3351970
Agustin Carthery <acarthery@Agustins-MacBook-Pro.local>
2021-12-18 20:55:11 -0300

    MS-118: Postulants components created

---
+++ src/Components/Postulant/Positions/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './positions.module.css';
+
+const Positions = () => {
+  return (
+    <section className={styles.container}>
+      <h2>Positions</h2>
+    </section>
+  );
+};
+
+export default Positions;
---
+++ src/Components/Postulant/Sessions/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './sessions.module.css';
+
+const Sessions = () => {
+  return (
+    <section className={styles.container}>
+      <h2>Sessions</h2>
+    </section>
+  );
+};
+
+export default Sessions;
