5a5edb55440e31a1be6c7ba3a0ca3e44be30ce11
Verona Baudracco <veronabaudracco@gmail.com>
2021-12-19 00:25:35 -0300

    MS-117: filter interviews list by postulant id

--- src/Components/Postulant/Interviews/index.js
+++ src/Components/Postulant/Interviews/index.js
@@ -1,12 +1,93 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styles from './interviews.module.css';
+import Modal from 'Components/Shared/Modal';
+import Button from 'Components/Shared/Button';
+import Table from 'Components/Shared/Table';
+import { useDispatch, useSelector } from 'react-redux';
+import { getInterviews, deleteInterview } from 'redux/interviews/thunks';
+import { cleanError } from 'redux/interviews/actions';
 
-const Interviews = () => {
+function Interviews() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedIdInterview, setIdInterview] = useState(false);
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const interviews = useSelector((store) => store.interviews.list);
+  const interviewsOfOnePostulant = interviews.filter(
+    (interview) => interview.postulant._id === '61adf3f49a63822458f2d7f0'
+  );
+
+  console.log(interviewsOfOnePostulant);
+
+  const error = useSelector((store) => store.interviews.error);
+  const isLoading = useSelector((store) => store.interviews.isFetching);
+
+  useEffect(() => {
+    if (!interviews.length) {
+      dispatch(getInterviews());
+    }
+  }, []);
+
   return (
     <section className={styles.container}>
-      <h2>Interviews</h2>
+      <h2 className={styles.title}>My Interviews</h2>
+      {isLoading ? (
+        <p className={styles.loading}>On Loading ...</p>
+      ) : (
+        <Table
+          columns={[
+            { name: 'Client', value: 'client.name' },
+            { name: 'Status', value: 'status' },
+            { name: 'Date', value: 'date' }
+          ]}
+          data={interviewsOfOnePostulant}
+          actions={[
+            {
+              text: 'Cancel',
+              callback: (e, item) => {
+                e.stopPropagation();
+                setIdInterview(item._id);
+                setShowModal(true);
+              }
+            }
+          ]}
+        />
+      )}
+      <Modal
+        show={showModal}
+        title="Do you want to proceed and delete this Interview?"
+        isLoading={isLoading}
+        cancel={{
+          text: 'Cancel',
+          callback: () => setShowModal(false)
+        }}
+        confirm={{
+          text: 'Confirm',
+          callback: () => {
+            dispatch(deleteInterview(selectedIdInterview)).then(() => {
+              setIdInterview(undefined);
+              setShowModal(false);
+            });
+          }
+        }}
+      />
+      <Modal
+        showModal={!!error}
+        title="Error"
+        message={error}
+        cancel={{
+          text: 'Close',
+          callback: () => dispatch(cleanError())
+        }}
+      />
+      <div className={styles.buttonContainer}>
+        <Button label="ADD INTERVIEW" onClick={() => history.push('/postulant/interviews/form')} />
+      </div>
     </section>
   );
-};
+}
 
 export default Interviews;
--- src/Routes/postulant.js
+++ src/Routes/postulant.js
@@ -2,7 +2,8 @@ import { Switch, Route, Redirect, useRouteMatch } from 'react-router-dom';
 import Home from 'Components/Postulant/Home';
 import Layout from 'Components/Layout';
 import Applications from 'Components/Postulant/Applications';
-import Interviews from 'Components/Postulant/Interviews';
+import InterviewsList from 'Components/Postulant/Interviews';
+import InterviewsForm from 'Components/Postulant/Interviews/Form';
 import Positions from 'Components/Postulant/Positions';
 import Sessions from 'Components/Postulant/Sessions';
 import Profile from 'Components/Postulant/Profile';
@@ -11,7 +12,7 @@ import SignUp from 'Components/Postulant/SignUp';
 const postulantRoutes = [
   { name: 'Home', path: '/postulant' },
   { name: 'Applications', path: '/postulant/applications' },
-  { name: 'Interviews', path: '/postulant/interviews' },
+  { name: 'Interviews', path: '/postulant/interviews/list' },
   { name: 'Positions', path: '/postulant/positions' },
   { name: 'Profile', path: '/postulant/profile' },
   { name: 'Sessions', path: '/postulant/sessions' },
@@ -25,7 +26,8 @@ const PostulantsRoutes = () => {
       <Switch>
         <Route exact path={`${url}/`} component={Home} />
         <Route path={`${url}/applications`} component={Applications} />
-        <Route path={`${url}/interviews`} component={Interviews} />
+        <Route path={`${url}/interviews/list`} component={InterviewsList} />
+        <Route path={`${url}/interviews/form`} component={InterviewsForm} />
         <Route path={`${url}/positions`} component={Positions} />
         <Route path={`${url}/sessions`} component={Sessions} />
         <Route path={`${url}/profile`} component={Profile} />
