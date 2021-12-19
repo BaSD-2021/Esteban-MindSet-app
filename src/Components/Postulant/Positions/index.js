965c81c1159c5b0007b2dcccd79ed2453d71d763
Verona Baudracco <veronabaudracco@gmail.com>
2021-12-19 19:43:08 -0300

    MS-120: delete onrow click redirection to form

--- src/Components/Postulant/Positions/index.js
+++ src/Components/Postulant/Positions/index.js
@@ -1,15 +1,12 @@
 import { useEffect, useState } from 'react';
 import styles from './positions.module.css';
 import Modal from 'Components/Shared/Modal';
-import Button from 'Components/Shared/Button';
-import { useHistory } from 'react-router-dom';
 import Table from 'Components/Shared/Table';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPositions } from 'redux/positions/thunks';
 import { cleanError } from 'redux/positions/actions';
 
 function Positions() {
-  const history = useHistory();
   const dispatch = useDispatch();
   const positions = useSelector((store) => store.positions.list);
   const error = useSelector((store) => store.positions.error);
@@ -46,13 +43,10 @@ function Positions() {
             { name: 'Professional Profile', value: 'professionalProfile.name' }
           ]}
           data={openPositions}
-          onRowClick={(item) => history.push(`/admin/positions/form?_id=${item._id}`)}
+          disableEdit={true}
           actions={[]}
         />
       )}
-      <div className={styles.buttonContainer}>
-        <Button label="ADD POSITION" onClick={() => history.push('/admin/positions/form')} />
-      </div>
     </section>
   );
 }
