5aea98b987aa33739aee9745a8949e26d1138403
Morabito Federico <morabitofj@gmail.com>
2022-01-03 16:18:31 -0300

    MS-162: add home route

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,16 +1,19 @@
 import { lazy, Suspense } from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
 
 const AdminRoutes = lazy(() => import('Routes/admin'));
 const PostulantRoutes = lazy(() => import('Routes/postulant'));
+const MainHomeRoutes = lazy(() => import('Routes/mainHome'));
 
 const Routes = () => {
   return (
     <Router>
       <Suspense fallback={<div />}>
         <Switch>
+          <Route path="/home" component={MainHomeRoutes} />
           <Route path="/postulant" component={PostulantRoutes} />
           <Route path="/admin" component={AdminRoutes} />
+          <Redirect to={`/home`} />
         </Switch>
       </Suspense>
     </Router>
