7284bd5713e488b48dbca6fc6374f1694debb178
Verona Baudracco <veronabaudracco@gmail.com>
2022-01-03 21:31:24 -0300

    MS-148: install dependencies

---
+++ src/redux/auth/reducer.js
@@ -0,0 +1,56 @@
+import {
+  LOGIN_PENDING,
+  LOGIN_SUCCESS,
+  LOGIN_ERROR,
+  CLEAN_ERROR,
+  SET_AUTHENTICATION
+} from './actions';
+
+const initialState = {
+  isLoading: false,
+  authenticated: false,
+  error: ''
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_PENDING: {
+      return {
+        ...state,
+        isFetching: true,
+        error: initialState.error
+      };
+    }
+    case LOGIN_SUCCESS: {
+      return {
+        ...state,
+        isFetching: false,
+        authenticated: true
+      };
+    }
+    case LOGIN_ERROR: {
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
+    }
+    case CLEAN_ERROR: {
+      return {
+        ...state,
+        error: initialState.error
+      };
+    }
+    case SET_AUTHENTICATION: {
+      return {
+        ...state,
+        authenticated: true
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default reducer;
