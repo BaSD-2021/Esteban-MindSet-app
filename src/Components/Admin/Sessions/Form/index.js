b012f0c9ec2ee185e8b5fd57ba7602d9d06aa352
Naila Ghigo <nailaghigo20@gmail.com>
2022-01-10 19:44:01 -0300

    MS-138: minor changes

--- src/Components/Admin/Sessions/Form/index.js
+++ src/Components/Admin/Sessions/Form/index.js
@@ -97,6 +97,9 @@ function sessionsForm() {
   };
 
   const validateDate = (value) => {
+    if (query.get('_id')) {
+      return undefined;
+    }
     if (required(value)) {
       return 'Required';
     }
@@ -106,7 +109,7 @@ function sessionsForm() {
     const now = new Date();
     const year = now.getFullYear();
     const month = now.getMonth() + 1 < 10 ? `0${now.getMonth() + 1}` : now.getMonth() + 1;
-    const date = now.getDate() < 10 ? `0${now.getDate()}` : '';
+    const date = now.getDate() < 10 ? `0${now.getDate()}` : now.getDate();
 
     let nowValue = Math.round(new Date(`${year}-${month}-${date}`).getTime() / 1000);
     return dateValue >= nowValue ? undefined : 'Invalid date';
--- src/Components/Postulant/Sessions/Form/index.js
+++ src/Components/Postulant/Sessions/Form/index.js
@@ -101,6 +101,9 @@ function sessionsForm() {
   };
 
   const validateDate = (value) => {
+    if (query.get('_id')) {
+      return undefined;
+    }
     if (required(value)) {
       return 'Required';
     }
@@ -110,7 +113,7 @@ function sessionsForm() {
     const now = new Date();
     const year = now.getFullYear();
     const month = now.getMonth() + 1 < 10 ? `0${now.getMonth() + 1}` : now.getMonth() + 1;
-    const date = now.getDate() < 10 ? `0${now.getDate()}` : '';
+    const date = now.getDate() < 10 ? `0${now.getDate()}` : now.getDate();
 
     let nowValue = Math.round(new Date(`${year}-${month}-${date}`).getTime() / 1000);
     return dateValue >= nowValue ? undefined : 'Invalid date';
