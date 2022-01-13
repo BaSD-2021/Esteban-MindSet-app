1355bac9b0c57bded00b2b61595fb8a822dfdec7
Esteban Frare <esteban.frare@radiumrocket.com>
2022-01-13 09:10:28 -0300

    MS-148: remove unused components

--- src/Components/Shared/Textarea/index.js
+++ src/Components/Shared/Textarea/index.js
@@ -1,33 +1,21 @@
 import React from 'react';
 import styles from './textarea.module.css';
 
-function Textarea({
-  title,
-  id,
-  name,
-  placeholder,
-  value,
-  onChange,
-  required,
-  rows,
-  cols,
-  disabled
-}) {
+function Textarea(props) {
+  const hasError = !!(props.meta.touched && props.meta.error);
   return (
     <div className={styles.container}>
-      <label className={styles.labelTitle}>{title}</label>
-      <input
-        className={styles.textareaContent}
-        id={id}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        required={required}
-        rows={rows}
-        cols={cols}
-        disabled={disabled}
-      />
+      <label className={styles.labelTitle}>{props.title}</label>
+      <textarea
+        className={`${styles.textareaContent} ${props.style} ${hasError && styles.textareaError}`}
+        name={props.name}
+        placeholder={props.placeholder}
+        maxLength={250}
+        rows={props.rows}
+        cols={props.cols}
+        {...props.input}
+      ></textarea>
+      <div className={styles.messageError}>{props.meta.touched && props.meta.error}</div>
     </div>
   );
 }
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,5 @@
 import { lazy, Suspense, useEffect } from 'react';
-import { Switch, BrowserRouter as Router, Redirect } from 'react-router-dom';
+import { Switch, BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 import { tokenListener } from 'helper/firebase';
 import PrivateRoute from './privateRoute';
 
@@ -7,6 +7,7 @@ const AdminRoutes = lazy(() => import('Routes/admin'));
 const PostulantRoutes = lazy(() => import('Routes/postulant'));
 const AuthRoutes = lazy(() => import('Routes/auth'));
 const PsychologistRoutes = lazy(() => import('Routes/psychologist'));
+const MainHomeRoutes = lazy(() => import('Routes/mainHome'));
 
 const Routes = () => {
   useEffect(() => {
