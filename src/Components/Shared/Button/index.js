d1b50de6fc53580f97aca230087c7ccb39908818
Agustin Carthery <acarthery@Agustins-MacBook-Pro.local>
2022-01-17 01:52:45 -0300

    MS-165: added hidden prop for handling issues and styles

--- src/Components/Shared/Button/index.js
+++ src/Components/Shared/Button/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import styles from './button.module.css';
 
-function Button({ label, onClick, disabled, type = 'button', theme = 'primary', style }) {
+function Button({ label, onClick, disabled, type = 'button', theme = 'primary', style, hidden }) {
   return (
     <div>
       <button
         type={type}
-        className={`${styles[theme]} ${style} ${disabled && styles.disabled}`}
+        className={`${styles[theme]} ${style} ${disabled && styles.disabled} ${
+          hidden && styles.hidden
+        }`}
         onClick={onClick}
         disabled={disabled}
+        hidden={hidden}
       >
         {label}
       </button>
